6870163730b0168d768f4b90df3afe6a36331f94
Sávio_Francisco <saviofrancisco150@hotmail.com>
2022-05-24 20:51:46 -0300

    esboço

--- login.php
+++ login.php
@@ -3,6 +3,19 @@
     if (isset($_GET) && isset($_GET['error'])) {
         $msgErr = $_GET['error'];
     }
+    /*include './lib/validate.php';
+
+    if(isset($_POST['username']) && isset($_POST['password'])){
+
+        $user = htmlspecialchars($_POST['username']);
+        $password = md5(htmlspecialchars($_POST['password']));
+    
+        if(loginCompare($user, $password) === 1){
+            header('Location: ./home.php ');
+        }else{
+            header('Location: ./login.php?login=erro ');
+        }
+    }*/
 ?>
 
 <!DOCTYPE html>
