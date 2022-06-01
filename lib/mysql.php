65328ad5f77ebd214f0ab8c2da2531626bb7034a
Cláudia Aguilar <aguilarp222@gmail.com>
2022-05-31 21:37:16 -0300

    up

--- lib/mysql.php
+++ lib/mysql.php
@@ -13,4 +13,27 @@
         return $mysqli;
     }
     }
+
+    function logar($usuario, $senha){
+        $query = "SELECT id, login, password FROM users WHERE login = '$usuario' and password = '$senha'";
+         $link = conecta();
+        if($link !== NULL){
+            $result = mysqli_query($link, $query);
+ 
+        //print_r($result);
+         //print_r($usuario); teste para verificar o que estou recebendo
+         //print_r($query);
+         if(mysqli_num_rows($result)<1){
+            //unset($_SESSION['usuario']); //se não tiver esta variável ele destroi a sessão
+            //unset($_SESSION['senha']);
+            header('Location: ../login.php');
+         }else{
+            //$_SESSION['usuario'] = $usuario;
+            //$_SESSION['senha'] = $senha;
+            //$_SESSION['nome'] = $nome;
+             header('Location: ../bemvindo.php');
+
+         }
+        }
+    }
 ?>
--- lib/valida.php
+++ lib/valida.php
@@ -1,6 +1,6 @@
 <?php
     
-     session_start();
+     //session_start();
 
      if(!empty($_POST['login']) && !empty($_POST['password']))
     {
@@ -8,33 +8,15 @@
         $usuario = $_POST['login'];
         $password = ($_POST['password']);
         $senha = md5($password);
-        $nome = $_POST['nome'];
+        //$nome = $_POST['nome'];
         //print_r('Email: ' . $login); teste para  verificar o que estou recebendo
          //print_r('<br>');
          //print_r('Senha: ' . $password);
-         //print_r('Senha: ' . $md5Password);
+         print_r('Senha: ' . $md5Password);
 
-         $query = "SELECT id, login, password FROM users WHERE login = '$usuario' and password = '$senha'";
-         $link = conecta();
-        if($link !== NULL){
-            $result = mysqli_query($link, $query);
- 
-        //print_r($result);
-         //print_r($usuario); teste para verificar o que estou recebendo
-         //print_r($query);
-         if(mysqli_num_rows($result)<1){
-            unset($_SESSION['usuario']); //se não tiver esta variável ele destroi a sessão
-            unset($_SESSION['senha']);
-            header('Location: ../login.php');
-         }else{
-            $_SESSION['usuario'] = $usuario;
-            $_SESSION['senha'] = $senha;
-            $_SESSION['nome'] = $nome;
-             header('Location: ../bemvindo.php');
-
-         }
-        }
+         
      }
+  logar($usuario, $senha);
   
      
 ?>
