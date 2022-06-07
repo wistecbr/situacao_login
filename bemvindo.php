a994779fe8d6320b39c2428200b36269f6547be1
BrytanniADJ <99222703+BrytanniADJ@users.noreply.github.com>
2022-06-07 17:20:38 -0300

    up

--- bemvindo.php
+++ bemvindo.php
@@ -1,51 +1,36 @@
 <?php
-    login();
-
-    if((!isset($_SESSION['usuario'])== true)and (!isset($_SESSION['senha']) == true)){
-        unset($_SESSION['usuario']);
-        unset($_SESSION['senha']);
-        header('Location: ./login.php ');
-    }else{
-        $logado = $_SESSION['usuario'];
-    }
-
-    $users = '';
-    if (isset($_GET) && isset($_GET['nome'])) {
-        $users = $_GET['nome'];
+    $user = '';
+    if (isset($_GET) && isset($_GET['username'])) {
+        $user = $_GET['username'];
     }
 ?>
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="./assests/css/reset.css">
-        <link rel="stylesheet" href="./assests/css/stilo.css">
-        <script src="./assests/js/script.js" defer></script>
-        <title>Bem vindo</title>
-    </head>
-    <body>
-        <header>
-            <figure>
-                <img src="" alt="logo">
-            </figure>
-            <ul>
-                <li> <a href="./">Bem Vindo</a></li>
-            </ul>
-        </header>
-        <main>
-
-            <?php
-
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assests/css/reset.css">
+    <link rel="stylesheet" href="./assets/css/estilo.css">
+    <script src="./assets/js/script.js" defer></script>
+    <title>Bem Vindo</title>
+</head>
+<body>
+    <header>
+        <h1>Sistema Login</h1>
+        <ul>
+            <li> <a href="./">Home</a></li>
+        </ul>
+    </header>
+    <main>
+        <?php
             if ($user !== '') {
                 echo '<h1> Bem-Vindo </h1>';
-                echo '<h2>'.$user.'</h2>';
+                echo '<h2>' . $user . '</h2>';
             }
-            ?>
-
-        </main>
-        <footer>
-        </footer>
-    </body>
+        ?>
+    </main>
+    <footer>
+    </footer>
+</body>
 </html>
--- login.php
+++ login.php
@@ -1,33 +1,29 @@
 <?php
-
-
     $msgErr = '';
     if (isset($_GET) && isset($_GET['error'])) {
         $msgErr = $_GET['error'];
     }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/reset.css">
     <link rel="stylesheet" href="./assets/css/estilo.css">
-    <script src="./assests/js/script.js" defer></script>
-    <title>Log-In</title>
+    <script src="./assets/js/script.js" defer></script>
+    <title>Login</title>
 </head>
 <body>
     <header>
-        <h1>Log-In</h1>
+        <h1>Sistema Login</h1>
         <ul>
             <li> <a href="./">Home</a></li>
         </ul>
     </header>
     <main>
-        <section>
+        <section class="principal">
             <form action="./lib/valida.php" method="post" enctype="multipart/form-data">
                 <p>
                     <label> Login: </label>
@@ -39,22 +35,21 @@
                     <input name="password" type="password" id="box_ano">
                 </p>
             <?php
-            if ($msgErr !== ''){
-                echo '<p>';
-                echo '<label> Erro login/senha ' . $msgErr . '</label>';
-                echo '</p>';
+                if ($msgErr !== '') {
+                    echo '<p>';
+                    echo '<label> Erro login/senha ' . $msgErr . '</label>';
+                    echo '</p>';
                 }
             ?>
             <p class="botao">
                 <input class="btn" type="submit" value="Entrar">
                 <input class="btn" type="button" value="Cancelar" onclick="bt_cancelar()">
             </p>
+            <p class="cadastro">Ainda não é inscrito: <a href="./cadastra.php">Cadastra-se!</a></p>
             </form>
         </section>
     </main>
     <footer>
-
     </footer>
 </body>
-
 </html>
