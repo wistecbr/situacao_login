917db2b4006b0ff7eb5c9e0db7052d0b43b4912e
Sávio_Francisco <saviofrancisco150@hotmail.com>
2022-06-28 20:34:54 -0300

    editing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,17 @@
 function deletar(id){
     let r = confirm("Confirmar remoção.");
 
-    if(r=== true){
+    if(r === true){
         window.location.assign('./lib/validate.php?deletar=' + id);
     }
 
     return;
-    
 }
 
 function editar(id){
     let r = confirm("Confirmar remoção.");
 
-    if(r=== true){
+    if(r === true){
         window.location.assign('./lib/validate.php?editar=' + id);
     }
     
