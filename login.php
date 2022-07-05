4041c60f77c9ef9e9dabcd3ae70f093cd6c90fce
BrytanniADJ <99222703+BrytanniADJ@users.noreply.github.com>
2022-07-05 19:36:32 -0300

    teste 4

--- login.php
+++ login.php
@@ -1,7 +1,8 @@
 <?php
     include './lib/valida.php';
+        $login = 0;
         $msgErr='';
-    if (isset($_GET) && isset($_GET['error'])) {
+    if (isset($_GET) && isset($_GET['error'])){
         $msgErr = $_GET['error'];
     }
 ?>
